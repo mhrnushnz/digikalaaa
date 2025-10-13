f5efaf60b39e80c53a75e7f65c542ede4ee12082
mehrnush_nz <mehrnush.npz@gmail.com>
2025-10-13 11:55:03 +0330

    main page digikalaa

---
+++ app/Livewire/Client/Auth/Index.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Livewire\Client\Auth;
+use App\Repositories\client\ClientAuthRepository;
+use Laravel\Socialite\Facades\Socialite;
+use Livewire\Component;
+
+class Index extends Component{
+
+    public function redirectToProvider(){
+        return Socialite::driver('google')->redirect();    //این میره وصل میشه به گوگل
+    }
+
+
+    public function handelProviderCallback(){
+        $repository = new ClientAuthRepository();     //به روش های تابع boot نمی تونیم از ریپازیتوری استفاده کنیم برای همین اینجوری از همون ریپازیتوری شی میسازیم!
+        $gmailUser = Socialite::driver('google')->stateless()->user();    //کاربر رو میسازه اکانت گوگلش رو
+        $repository->CheckUser($gmailUser);
+        return redirect()->route('home');
+    }
+
+
+    public function render(){
+        return view('livewire.client.auth.index')->layout('layouts.client.app');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Livewire\Admin\Payment\Index as IndexPayment;
+use App\Livewire\Admin\Category\Features;
+use App\Livewire\Admin\Product\CkUpload;
+use App\Livewire\Admin\Product\Content;
+use App\Livewire\Admin\Product\Features as ProductFeatures;
+use App\Livewire\Admin\Category\FeatureValue;
+use App\Livewire\Admin\Category\Index as CategoryIndex;
+use App\Livewire\Admin\City\Index as CityIndex;
+use App\Livewire\Admin\Country\Index as CountryIndex;
+use App\Livewire\Admin\Dashboard\Index as DashboardIndex;
+use App\Livewire\Admin\Product\Create;
+use App\Livewire\Admin\Product\Index;
+use App\Livewire\Admin\State\Index as StateIndex;
+use App\Livewire\Admin\Delivery\Index as DeliveryIndex;
+use App\Livewire\Client\Auth\Index as ClientIndex;
+
+
+
+// Admin routes
+Route::get('/', function (){
+    return view('home')->layout('layouts.admin.app');
+});
+Route::get('/dashboard', DashboardIndex::class)->name('admin.dashboard.index');
+Route::get('/country', CountryIndex::class)->name('admin.country.index');
+Route::get('/state', StateIndex::class)->name('admin.state.index');
+Route::get('/city', CityIndex::class)->name('admin.city.index');
+Route::get('/category', CategoryIndex::class)->name('admin.category.index');
+Route::get('/features/{category?}/{categoryId}', Features::class)->name('admin.features.index');
+Route::get('/category/features_value/{categoryFeature}', FeatureValue::class)->name('admin.features_value.index');
+Route::get('/product/create', Create::class)->name('admin.product_create.index');
+Route::get('/product/index', Index::class)->name('admin.product.index');
+Route::get('/product/features/{product}', ProductFeatures::class)->name('admin.product.features');
+Route::get('/product/content/{product}', Content::class)->name('admin.product.content');
+Route::get('/product/ck-upload/{productId}', CkUpload::class)->name('admin.product.ck-upload');
+Route::get('/delivery/index', DeliveryIndex::class)->name('admin.delivery.index');
+Route::get('/payment/index', IndexPayment::class)->name('admin.payment.index');
+
+
+
+//client
+Route::get('/', function (){
+    return view('home');
+});
+
+Route::get('/auth', ClientIndex::class)->name('client.auth.index');
+Route::get('/auth/gmail', [ClientIndex::class, 'redirectToProvider'])->name('gmail');
+Route::get('/auth/gmail/callback', [ClientIndex::class, 'handelProviderCallback'])->name('callback');
+
+
